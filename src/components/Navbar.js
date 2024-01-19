d550dce62861cf25d1a12167fa8148691285a33c
Gagan Arora <arorag2577@gmail.com>
2024-01-19 22:50:21 +0530

    navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import logo from "../Images/logo-black.svg";
-const CustomNavbar = () => {
+const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-light p-3 shadow-sm">
       <div className="container-fluid">
         <a class="navbar-brand" href="/">
-          <img src={logo} alt="Bootstrap" width="130" height="60" />
+          <img src={logo} alt="Logo" width="130" height="60" />
         </a>
         <button
           className="navbar-toggler"
@@ -23,21 +23,21 @@ const CustomNavbar = () => {
           id="navbarNavAltMarkup"
         >
           <div className="navbar-nav">
-            <a className="nav-link" aria-current="page" href="/">
-              Company search
+            <a className="nav-link m-3" aria-current="page" href="/">
+              <i class="fa-solid fa-magnifying-glass"></i> Company search
             </a>
-            <a className="nav-link" href="/">
-              Developer search
+            <a className="nav-link m-3" href="/">
+              <i class="fa-solid fa-laptop-file"></i> Developer search
             </a>
-            <a className="nav-link" href="/">
-              Company map
+            <a className="nav-link m-3" href="/">
+              <i class="fa-solid fa-location-dot"></i> Company map
             </a>
-            <a className="nav-link" href="/">
-              Knowledge center
+            <a className="nav-link m-3" href="/">
+              <i class="fa-solid fa-book"></i> Knowledge center
             </a>
-            <button href="/" className="btn btn-warning nav-btn">
+            {/* <button href="/" className="btn btn-warning btn-nav">
               Share Your Project
-            </button>
+            </button> */}
           </div>
         </div>
       </div>
@@ -45,4 +45,4 @@ const CustomNavbar = () => {
   );
 };
 
-export default CustomNavbar;
+export default Navbar;
