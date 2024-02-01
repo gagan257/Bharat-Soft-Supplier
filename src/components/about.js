54aab110cb6165cdf68d34321c42957dee39058b
Gagan Arora <arorag2577@gmail.com>
2024-02-01 22:52:21 +0530

    about us

--- src/components/about.js
+++ src/components/about.js
@@ -3,9 +3,22 @@ import React from "react";
 export default function about() {
   return (
     <div>
-      <div className="container">
+      <div
+        className="container mt-5 mb-5 text-center"
+        data-aos="slide-right"
+        data-aos-delay="500"
+        data-aos-once="true"
+      >
         <h3>
-          About Our <span className="aboutus-span">Company</span>
+          About Our{" "}
+          <span
+            className="aboutus-span"
+            data-aos="fade-right"
+            data-aos-delay="800"
+            data-aos-once="true"
+          >
+            Company
+          </span>
         </h3>
         <p className="mt-3">
           Every day, creative minds in the entrepreneurial and organizational
@@ -26,6 +39,37 @@ export default function about() {
           Whether you are in pursuit of new customers or on the quest for the
           perfect supplier, take advantage of the advantages offered by our
           premium services.
+        </p>
+        <h3>
+          <span
+            className="aboutus-span pt-5"
+            data-aos="fade-right"
+            data-aos-delay="800"
+            data-aos-once="true"
+          >
+            Contact
+          </span>{" "}
+          Us
+        </h3>
+        <p>
+          <i className="fa fa-phone"></i> Payal Gandhi: +31 644806659
+          <br />
+          <i className="fa fa-phone"></i> Team Coordinator: +31 644905433
+        </p>
+        <p>
+          <i className="fa-regular fa-envelope"></i>{" "}
+          hello@bellegirllifestyle.com
+        </p>
+        <p>
+          <i class="fa-solid fa-location-dot"></i> Klifrakplantsoen 178, 3544 RC
+          Utrecht. The Netherlands
+        </p>
+        <p>
+          KVK Number (COC): 80193935 <br />
+          VAT Number: NL003405066B77 <br />
+          A/c No (IBAN): BE90 9671 8679 9532 <br />
+          Account Holder: Bellegirl Lifestyle <br />
+          BIC: TRWIBEB1XXX
         </p>
       </div>
     </div>
